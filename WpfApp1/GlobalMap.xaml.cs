6e89b054a23c498129aa10421f171663f3295d29
Fishazei <fishazei@mail.ru>
2023-12-25 13:31:05 +0300

    Add project files.

---
+++ WpfApp1/GlobalMap.xaml.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    class HookMap
+    {
+        double x0, y0;
+        double xcur, ycur;
+        double l, m;
+        double force;
+
+        public Image MyImage { get; set; }
+        public Rectangle MyRectangle { get; set; }
+        Canvas myCanvas;
+
+        public HookMap(double mouseTop, double mouseLeft, double currentTop, double currentLeft, Canvas canvas, TimeSpan heldTime)
+        {
+            myCanvas = canvas;
+            y0 = currentLeft + 50;
+            x0 = currentTop + 50;
+
+            l = mouseTop - 10 - x0;
+            m = mouseLeft - 10 - y0;
+
+            //Задание крючка
+            MyRectangle = new Rectangle();
+
+            MyRectangle.Height = 20;
+            MyRectangle.Width = 20;
+            MyRectangle.Fill = new SolidColorBrush(Colors.AliceBlue);
+            Canvas.SetTop(MyRectangle, y0 - 10);
+            Canvas.SetLeft(MyRectangle, x0 - 10);
+
+            myCanvas.Children.Add(MyRectangle);
+
+            // Вычисление силы вылета в зависимости от времени зажатия
+            force = Math.Min(100, heldTime.TotalMilliseconds / 800);
+
+            //Эксперимент
+            Line line = new Line();
+            line.X1 = x0;
+            line.Y1 = y0;
+            line.X2 = l * force + x0;
+            line.Y2 = m * force + y0;
+            line.StrokeThickness = 4;
+            line.Stroke = new SolidColorBrush(Colors.AliceBlue);
+
+            myCanvas.Children.Add(line);
+            MessageBox.Show($"{line.X2}, {line.Y2}");
+
+            // Создание анимации перемещения
+            TranslateTransform translateTransform = new TranslateTransform();
+            MyRectangle.RenderTransform = translateTransform;
+
+            DoubleAnimation animationX = new DoubleAnimation
+            {
+                From = 0,
+                To = l * force,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            DoubleAnimation animationY = new DoubleAnimation
+            {
+                From= 0,
+                To = m * force,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+            animationX.IsAdditive = true;
+            animationY.IsAdditive = true;
+
+            translateTransform.BeginAnimation(TranslateTransform.XProperty, animationX);
+            translateTransform.BeginAnimation(TranslateTransform.YProperty, animationY);
+
+            xcur = l * force - 10 + x0;
+            ycur = m * force - 10 + y0;
+
+
+            
+        }
+
+        public void GoBack()
+        {
+            
+            xcur -= l * force * 0.01;
+            ycur -= m * force * 0.01;
+
+            MessageBox.Show($"{Canvas.GetTop(MyRectangle)}, {Canvas.GetLeft(MyRectangle)}");
+
+            Canvas.SetTop(MyRectangle, ycur);
+            Canvas.SetLeft(MyRectangle, xcur);
+
+            MessageBox.Show($"{Canvas.GetTop(MyRectangle)}, {Canvas.GetLeft(MyRectangle)}");
+        }
+    }
+    /// <summary>
+    /// Interaction logic for GlobalMap.xaml
+    /// </summary>
+    public partial class GlobalMap : Page
+    {
+        private const int TimerInterval = 16; // Примерно 60 FPS
+        private const int MoveSpeed = 5;
+
+        private DateTime keyDownTime;
+        private bool isSpaceKeyDown;
+        private bool isThrHook;
+
+        private HookMap myHook;
+
+        private readonly DispatcherTimer timer;
+
+        public GlobalMap()
+        {
+            InitializeComponent();
+
+            CreateTexture();
+
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(TimerInterval)
+            };
+            timer.Tick += MovePlayer;
+            timer.Tick += HookThr;
+            isSpaceKeyDown = false;
+            isThrHook = false;
+            Loaded += YourPage_Loaded;
+            timer.Start();
+        }
+
+        private async void YourPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await GameLoop();
+        }
+        public void MovePlayer(object sender, EventArgs e)
+        {
+            double currentLeft = Canvas.GetLeft(movingRectangle);
+            double currentTop = Canvas.GetTop(movingRectangle);
+
+            if (!isThrHook && Keyboard.IsKeyDown(Key.W) && currentTop - MoveSpeed > 100)
+            {
+                Canvas.SetTop(movingRectangle, currentTop - MoveSpeed);
+            }
+            if (!isThrHook && Keyboard.IsKeyDown(Key.S) && currentTop + movingRectangle.Width + MoveSpeed < 655)
+            {
+                Canvas.SetTop(movingRectangle, currentTop + MoveSpeed);
+            }
+            if (!isThrHook && Keyboard.IsKeyDown(Key.D) && currentLeft + movingRectangle.Width + MoveSpeed < 185)
+            {
+                Canvas.SetLeft(movingRectangle, currentLeft + MoveSpeed);
+            }
+            if (!isThrHook && Keyboard.IsKeyDown(Key.A) && currentLeft - MoveSpeed > 10)
+            {
+                Canvas.SetLeft(movingRectangle, currentLeft - MoveSpeed);
+            }
+            //Начали замахиваться
+            if (Keyboard.IsKeyDown(Key.Space) && !isSpaceKeyDown)
+            {
+                isSpaceKeyDown = true;
+                keyDownTime = DateTime.Now;
+            }
+            //Тянем назад
+            if (isThrHook && Keyboard.IsKeyDown(Key.E))
+            {
+                myHook.GoBack();
+            }
+        }
+        //Кинули крюк
+        public void HookThr(object sender, EventArgs e)
+        {
+            if(isSpaceKeyDown && !isThrHook && Keyboard.IsKeyUp(Key.Space))
+            {
+                isThrHook = true;
+                isSpaceKeyDown = false;
+
+                double currentLeft = Canvas.GetLeft(movingRectangle);
+                double currentTop = Canvas.GetTop(movingRectangle);
+
+                double mouseLeft = Mouse.GetPosition(GlobalMap1).X;
+                double mouseTop = Mouse.GetPosition(GlobalMap1).Y;
+
+                TimeSpan heldTime = DateTime.Now - keyDownTime;
+
+                myHook = new HookMap(
+                    mouseLeft,
+                    mouseTop,
+                    currentLeft,
+                    currentTop,
+                    MyCanvas,
+                    heldTime
+                    );
+            }
+        }
+
+        private async Task GameLoop()
+        {
+            while (true)
+            {
+                await Task.Delay(10);
+            }
+        }
+
+        private void CreateTexture()
+        {
+            MapGenerator mapGenerator = new MapGenerator(15,22);
+            // Устанавливаем текстуру как источник изображения для Image
+            matrixImage.Source = mapGenerator.CreateTexture();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1
+{
+    class MapGenerator
+    {
+        public int[,] matrix;
+
+        public MapGenerator(int x = 30, int y = 40) 
+        {
+            matrix = Generate(x, y);
+        }
+
+        /// <summary>
+        /// Генерирование карты
+        /// </summary>
+        /// <param name="x">Размер по горизонтале</param>
+        /// <param name="y">Размер по вертикале</param>
+        /// <returns>Возращает матрицу int с размерами x+2 y+2, рабочей частью является область от [1,1] до [x+1,y+1]</returns>
+        private int[,] Generate(int x, int y)
+        {
+            int tile;
+            Random random = new Random();
+
+            //Вычисление итоговой матрицы
+            int[,] RMap = new int[x + 2, y + 2];
+            for (int i = 1; i < x + 1; i++)
+                for (int j = 1; j < y + 1; j++)
+                {
+                    if (RMap[i, j] == 0)
+                    {
+                        tile = random.Next(100);
+                        RMap[i, j] = tile <= 5 && j < y ? 9 : tile <= 40 && tile >= 30 ? 6 : tile <= 25 && tile >= 10 ? 3 : 0;
+
+                        //Установка коряг
+                        if (RMap[i, j] == 9)
+                        {
+                            RMap[i, j + 1] = -1;
+
+                            RMap[i - 1, j - 1] = random.Next(20) <= 2 ? -1 : RMap[i - 1, j - 1];
+                            RMap[i - 1, j]     = random.Next(20) <= 2 ? -1 : RMap[i - 1, j];
+                            RMap[i - 1, j + 1] = random.Next(20) <= 2 ? -1 : RMap[i - 1, j + 1];
+                            RMap[i, j - 1]     = random.Next(20) <= 1 ? -1 : RMap[i, j - 1];
+                            RMap[i, j + 1]     = random.Next(20) <= 2 ? -1 : RMap[i, j + 1];
+                            RMap[i + 1, j - 1] = random.Next(20) <= 2 ? -1 : RMap[i + 1, j - 1];
+                            RMap[i + 1, j + 1] = random.Next(20) <= 2 ? -1 : RMap[i + 1, j + 1];
+                        }
+                    }
+                }
+            //Сглаживание
+            for (int i = 1; i < x + 1; i++)
+                for (int j = 1; j < y + 1; j++)
+                {
+                    if (RMap[i, j] == 0)
+                    {
+                        RMap[i, j] = Math.Abs((RMap[i + 1, j] + RMap[i, j + 1] + RMap[i - 1, j] + RMap[i, j - 1]) / 3);
+                    }
+                }
+            for (int i = 1; i < x + 1; i++)
+            {
+                RMap[i, 0] = Math.Abs(RMap[i, 1] + RMap[i + 1, 1] + RMap[i - 1, 1]) / 3;
+                RMap[i, y+1] = Math.Abs(RMap[i, y] + RMap[i + 1, y] + RMap[i - 1, y]) / 3;
+            }
+            for (int i = 1; i < y + 1; i++)
+            {
+                RMap[0, i] = Math.Abs(RMap[1, i] + RMap[1,i + 1] + RMap[1, i - 1]) / 3;
+                RMap[x+1, i] = Math.Abs(RMap[x, i] + RMap[x, i + 1] + RMap[x, i - 1]) / 3;
+            }
+            return RMap;
+        }
+
+        /// <summary>
+        /// Создание текстуры водоёма
+        /// </summary>
+        /// <param name="PixelBlockSize">масштабирование, по умолчанию один квадрат - 50 пикселей</param>
+        /// <returns>Возвращает текстуру водоёма</returns>
+        public WriteableBitmap CreateTexture(int PixelBlockSize = 20)
+        {
+
+            // Размеры текстуры в пикселях, умноженные на размер блока
+            int textureWidth = matrix.GetLength(1) * PixelBlockSize;
+            int textureHeight = matrix.GetLength(0) * PixelBlockSize;
+
+            // Создаем WriteableBitmap для текстуры
+            WriteableBitmap texture = new WriteableBitmap(textureWidth, textureHeight, 96, 96, PixelFormats.Bgr32, null);
+
+            // Заполняем текстуру на основе значений матрицы
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    int value = matrix[i, j];
+                    Color color = GetColor(value);
+
+                    // Заполняем область nxn пикселей цветом
+                    for (int y = 0; y < PixelBlockSize; y++)
+                    {
+                        for (int x = 0; x < PixelBlockSize; x++)
+                        {
+                            int pixelX = j * PixelBlockSize + x;
+                            int pixelY = i * PixelBlockSize + y;
+
+                            // Задаем цвет пикселя в текстуре
+                            texture.WritePixels(new Int32Rect(pixelX, pixelY, 1, 1), new byte[] { color.B, color.G, color.R, 255 }, 4, 0);
+                        }
+                    }
+                }
+            }
+
+            return texture;
+        }
+
+        //Генерация цвета
+        private Color GetColor(int value)
+        {
+            Color color;
+            if (value == -1) color = Colors.Brown;
+            else
+            {
+                color.A = 255;
+                color.B = (byte)(-4.4 * value + 171);
+                color.G = (byte)(-3.3 * value + 130);
+                color.R = (byte)(-0.074 * Math.Pow(value, 3) + 0.944 * Math.Pow(value, 2) - 3.833 * value + 57);
+            }
+            return color;
+        }
+    }
+    
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+ 
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GlobalMap globalMap = new GlobalMap();
+
+            MainFrame.Content = globalMap;
+        }
+
+    }
+}
